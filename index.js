aeb59323a5f2168ff32444ca4cca85ee8140cffd
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-26 09:36:24 -0300

    se hacen las configuraciones iniciales

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import * as dotenv from 'dotenv';
 
+dotenv.config(); // puedo leer variables de entorno, se declara antes del app.set
 //crear una instancia de express
 const app = express();
 
@@ -8,3 +12,16 @@ app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
     console.log(`Estoy en el puerto ${app.get('port')}`)
 })
+
+//middlewares: funciones que ejecutan alguna tarea antes de llegar a las rutas
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //Permite interpretar el formato json del body de una solicitud(put,post, etc)
+app.use(express.urlencoded({extended: true})); //permite en el request object, strings y arrays
+app.use(morgan('dev')); // nos da informacion extra en la terminal
+
+
+//rutas
+//http://localhost:4000/prueba
+app.get('/prueba', (req, res)=>{
+    res.send('esto es una prueba de la solicitud get a mi backend');
+});
