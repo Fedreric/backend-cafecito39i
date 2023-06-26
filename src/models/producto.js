fd58a1b9220374db56eb4c1370a1d18aec7f4f47
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-26 10:24:52 -0300

    Se agrega el modelo del producto para la db

---
+++ src/models/producto.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto: {
+        type: String,
+        minLength: 2,
+        maxLength:100,
+        unique: true,
+        required: true
+    },
+    precio:{
+        type:Number,
+        min:1,
+        max:20000,
+        required:true
+    },
+    imagen:{
+        type: String,
+        required:true
+    },
+    categoria:{
+        type: String,
+        required:true
+    },
+    descripcion: {
+        type: String,
+        minLength: 10,
+        maxLength:400,
+        required: true
+    }
+})
+
+const Producto = model('producto', productoSchema)
+
+export default Producto;
