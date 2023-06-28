d6a37e5edf6401005ae7bfafea36ca654ec323c0
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-28 10:44:07 -0300

    Agregamos la ruta y controlador para obtener  un producto por id

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,61 +1,73 @@
-import Producto from "../models/producto"
+import Producto from "../models/producto";
 
 //Read de productos
 export const obtenerProductos = async (req, res) => {
-    try {
-        const productos = await Producto.find();
-        res.status(200).json(productos)
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            mensaje: 'Error al buscar los productos'
-        })
-    }
-}
+  try {
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al buscar los productos",
+    });
+  }
+};
 
 //create productos
 
 export const crearProducto = async (req, res) => {
-    try {
-        const productoNuevo = new Producto(req.body)
-        await productoNuevo.save()
-        res.status(201).json({
-            mensaje:'El producto fue creado conrrectamente!'
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(404).json({
-            mensaje: 'Error al crear el producto'
-        })
-    }
-}
+  try {
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "El producto fue creado conrrectamente!",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al crear el producto",
+    });
+  }
+};
 
-export const borrarProducto = async (req, res) =>{
-    try {
-        //obtener el id y luego borrar
-        await Producto.findByIdAndDelete(req.params.id);
-        res.status(200).json({
-            mensaje: 'Producto eliminado con exito'
-        });
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({
-            mensaje: 'Error al eliminar el producto'
-        });
-    }
-}
+export const borrarProducto = async (req, res) => {
+  try {
+    //obtener el id y luego borrar
+    await Producto.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      mensaje: "Producto eliminado con exito",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al eliminar el producto",
+    });
+  }
+};
 
-export const editarProducto = async (req,res)=> {
-    try {
-        // Extraer el id del request y el body
-        await Producto.findByIdAndUpdate(req.params.id, req.body);
-        res.status(200).json({
-            mensaje: 'Producto editado correctamente'
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(404).json({
-            mensaje: 'No se pudo editar el producto'
-        });
-    }
-}
+export const editarProducto = async (req, res) => {
+  try {
+    // Extraer el id del request y el body
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      mensaje: "Producto editado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "No se pudo editar el producto",
+    });
+  }
+};
+
+export const obtenerProducto = async (req, res) => {
+  try {
+    const producto = await Producto.findById(req.params.id);
+    res.status(200).json(producto);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "no se pudo obtener el producto",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,21 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, obtenerProductos } from "../controllers/productos.controllers";
-const router = Router() 
+import {
+  borrarProducto,
+  crearProducto,
+  editarProducto,
+  obtenerProducto,
+  obtenerProductos,
+} from "../controllers/productos.controllers";
+const router = Router();
 
 // app.get('/prueba', (req, res)=>{
 //     res.send('esto es una prueba de la solicitud get a mi backend');
 // });
 
-router.route('/productos').get(obtenerProductos).post(crearProducto);
-router.route('/productos/:id').delete(borrarProducto).put(editarProducto);
-export default router; 
+router.route("/productos").get(obtenerProductos).post(crearProducto);
+router
+  .route("/productos/:id")
+  .delete(borrarProducto)
+  .put(editarProducto)
+  .get(obtenerProducto);
+export default router;
